0ca8b004de53c1c8e7e831198c71e2368de0aba5
Paulo Meneghini <paulo.meneghini@outlook.com>
2024-04-18 20:17:57 -0300

    Changing the login to Main and readjusting the layout

--- app/src/main/java/com/example/controledeponto/MainActivity.kt
+++ app/src/main/java/com/example/controledeponto/MainActivity.kt
@@ -3,17 +3,63 @@ package com.example.controledeponto
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnLogin = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.btn_entrar1)
+        auth = Firebase.auth
+
+        val btnLogin = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.btn_login)
 
         btnLogin.setOnClickListener {
-            val goToLogin = Intent(this, LoginActivity::class.java)
-            startActivity(goToLogin)
+            val email = findViewById<EditText>(R.id.loginEmail).text.toString()
+            val password = findViewById<EditText>(R.id.loginPassword).text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()) {
+                signInWithEmailAndPassword(email, password)
+                val goToMenu = Intent(this@MainActivity, CalendarActivity::class.java)
+
+                val user = auth.currentUser
+
+                goToMenu.putExtra("USER_EMAIL", user?.email)
+                goToMenu.putExtra("USER_NAME", user?.displayName)
+                goToMenu.putExtra("USER_ID", user?.providerId)
+
+                startActivity(goToMenu)
+            } else {
+                Toast.makeText(baseContext, "Por favor, preencha todos os campos", Toast.LENGTH_LONG).show()
+            }
         }
+
+    }
+
+    private fun signInWithEmailAndPassword(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if(task.isSuccessful) {
+                Log.d(TAG, "singInWithEmailAndPassword:Sucess")
+                val user = auth.currentUser
+                Log.d(TAG, "$user")
+
+                Toast.makeText(baseContext, "Logado com Sucesso! Usuário: $user", Toast.LENGTH_LONG).show()
+
+
+            } else {
+                Log.w(TAG, "singInWithEmailAndPassword:Sucess", task.exception)
+                Toast.makeText(baseContext, "Authenication Failure", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    companion object {
+        private var TAG = "EmailAndPassword"
     }
 }
